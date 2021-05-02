be2aaa3155014bc4925930471dc70037a395f5ba
Sherline Kinyanjui <sherlinekinyanjui@gmail.com>
2021-05-02 16:19:35 +0300

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -87,7 +87,6 @@ def authorized():
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
-        # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(request.args['code'],scopes=Config.SCOPE,redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         if "error" in result:
             return render_template("auth_error.html", result=result)
@@ -114,24 +113,23 @@ def logout():
     return redirect(url_for('login'))
 
 def _load_cache():
-    # TODO: Load the cache from `msal`, if it exists
-     cache = msal.SerializableTokenCache()
+    cache = msal.SerializableTokenCache()
     if session.get('token_cache'):
         cache.deserialize(session['token_cache'])
     return cache
 
 def _save_cache(cache):
-    # TODO: Save the cache, if it has changed
     if cache.has_state_changed:
         session['token_cache'] = cache.serialize()
-        
 
 def _build_msal_app(cache=None, authority=None):
-    # TODO: Return a ConfidentialClientApplication
     return msal.ConfidentialClientApplication(
     Config.CLIENT_ID, authority=authority,
-    client_credential=Config.CLIENT_SECRET, token_cache=cache
+    client_credential=Config.CLIENT_SECRET, token_cache=cache)
+
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-    # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return _build_msal_app(authority=authority).get_authorization_request_url(scopes,state=state,redirect_uri=url_for('authorized', _external=True, _scheme='https'))
+    return _build_msal_app(authority=authority).get_authorization_request_url(
+    scopes,
+    state=state,
+    redirect_uri=url_for('authorized', _external=True, _scheme='https'))
